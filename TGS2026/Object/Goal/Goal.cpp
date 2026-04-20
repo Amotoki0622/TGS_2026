e53ecfb339abc8eb3f61fee6a58363ded33765dd
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2026-04-20 12:35:23 +0900

    あ

--- TGS2026/Object/Goal/Goal.cpp
+++ TGS2026/Object/Goal/Goal.cpp
@@ -1,9 +1,13 @@
 #include "Goal.h"
+#include "../Player/Player.h"
+#include "../../Utility/CollisionManager.h"
 #include "DxLib.h"
 
 void Goal::Initialize()
 {
-
+	pos = { 280, 570 };   // íÜêSç¿ïW
+	width = 80;
+	height = 80;
 }
 
 void Goal::Update()
@@ -14,6 +18,7 @@ void Goal::Update()
 void Goal::Draw() const
 {
 	DrawBox(240, 530, 320, 610, GetColor(255, 255, 255), true);
+	//DrawFormatString(10, 100, GetColor(255, 255, 255), "x: %.2f y: %.2f", player.x, player.y);
 }
 
 void Goal::Finalize()
