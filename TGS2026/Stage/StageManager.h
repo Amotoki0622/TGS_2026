61a5b16c559dacaa07eb4bc17b4f5db3636dd2ee
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2026-05-25 15:24:23 +0900

    更新(マージ修正)
    StageManagerの修正
    IngameSceneのマージの修正を直しました。

--- TGS2026/Stage/StageManager.h
+++ TGS2026/Stage/StageManager.h
@@ -2,6 +2,8 @@
 
 #include "StageData.h"
 #include "StageLoader.h"
+#include "../Object/GameObject.h"
+#include "../Object/Trap/TrapObject/TrapObject.h"
 
 #include <vector>
 class StageManager
@@ -10,6 +12,8 @@ private:
 	std::vector<StageData> m_allStages; // 全ステージデータ
 	int m_currentLevel;                 // 現在のステージ番号
 	int m_currentMoveLimit;             // 現在の残り手数
+
+	std::vector<GameObject*> m_Object;
 
 public:
 	// インストラクタ
@@ -46,12 +50,56 @@ public:
 		return (int)m_allStages.size();
 	}
 
+	const std::vector<GameObject*>& GetGenerateObject() const
+	{
+		return m_Object;
+	}
+
+	// クリアする関数(ステージ切り替え時用)
+	void ClearObjects();
 
 private:
 	// 実際にオブジェクトを生成する内部関数
 	void CreateStageObject();
 
+	// プレイヤーの初期位置を保存する変数
+	Vector2D m_playerSpawnPos = Vector2D(-100.0f, -100.0f);
+
 	// 現在プレイ中のステージデータを参照(ポインタ)
 	StageData* m_pCurrentData;
+
+	bool  m_debug_isB_Detected = false; // 'B' が見つかったら true にする
+	float m_debug_bX = 0.0f;           // 見つかったX座標
+	float m_debug_bY = 0.0f;
+
+public:
+	template <class T>
+	T* CreateStageObjectInstance(const Vector2D& generate_location)
+	{
+		T* new_instance = new T();
+
+		GameObject* new_object = dynamic_cast<GameObject*>(new_instance);
+
+		if (new_object == nullptr)
+		{
+			delete new_instance;
+			throw std::string("ゲームオブジェクトを生成できませんでした。");
+			return nullptr;
+		}
+
+		new_object->SetLocation(generate_location);
+
+		new_object->Initialize();
+
+		m_Object.push_back(new_object);
+
+		return new_instance;
+	}
+
+	// InGameSceneにプレイヤーの位置を渡す関数
+	Vector2D GetPlayerSpawnPosition() const;
+
+	// デバック
+	void DrawDebugInfo() const;
 };
 
