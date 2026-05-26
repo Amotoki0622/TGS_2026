0af8b1b870194e6cd7773db2bad570ff8fe4c012
kouta01 <138412533+kouta01@users.noreply.github.com>
2026-05-26 15:06:18 +0900

    ワープ位置の変更

--- TGS2026/Scene/InGame/InGameScene.cpp
+++ TGS2026/Scene/InGame/InGameScene.cpp
@@ -73,7 +73,7 @@ void InGameScene::Initialize()
   
 	allObjects.push_back(new Key(250.0f, 250.0f, &player));
 
-	allObjects.push_back(new Warp(328.0f, 300.0f, 128.0f, 128.0f, 900.0f, 320.0f));
+	allObjects.push_back(new Warp(328.0f, 300.0f, 128.0f, 128.0f, 832.0f, 250.0f));
 	
 
 	// 3. プレイヤーの初期化
